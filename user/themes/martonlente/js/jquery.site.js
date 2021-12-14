d8bc2d50179018b3523bd8467e4edcd7c6dca983
Márton Lente <lentemarton@gmail.com>
2021-12-14 16:54:24 +0100

    Add js link top transform rotate base

--- user/themes/martonlente/js/jquery.site.js
+++ user/themes/martonlente/js/jquery.site.js
@@ -1,7 +1,17 @@
 $(function() {
+  var $window = $(window);
+
   /*Svg injector
    */
   var $svgInjectorItems = $('.js-svg-injector-item');
 
   SVGInjector($svgInjectorItems);
+
+  var $linkTop = $('.js-link-top');
+
+  $window.scroll(function() {
+    var scrollTopCurrent = $window.scrollTop();
+
+    $linkTop.css('transform', 'rotate(' + scrollTopCurrent + 'deg)');
+  });
 });
